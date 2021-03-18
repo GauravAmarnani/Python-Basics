6a27f98af77ac6add96bce1abb3860f9980e05c0
GauravAmarnani <gauravamarnani1213@gmail.com>
2021-03-18 20:24:08 +0530

    Added information about data types into the section1.txt and wrote programs for int and float data types to explain with example in a better way.

---
+++ com/byGaurav/dataTypes/forFloat.py
@@ -0,0 +1,4 @@
+floatValue = 1.2345678982398794873758734893;
+print("A Float Value : ",floatValue);
+floatValueUsingExponential = 1.2e10;
+print("A Float Value using Exponential : ", floatValueUsingExponential);
